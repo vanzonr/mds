18f4d644b57063de69e7d4d3b80614f225f61bfa
Ramses van Zon <rzon@scinet.utoronto.ca>
2026-08-13 13:55:21 -0400

    better cleanup, checking the engine, checking catsub, fixed support for legacy LOGOONTITLE

--- testall.sh
+++ testall.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -exuo pipefail
 mkdir -p testallresults
-for i in $(find themes -mindepth 1 -maxdepth 1 -type d); do
-    ./mds -v -s "$i" template.md
-    ./mds -vt -s "$i" template.md
+find themes -mindepth 1 -maxdepth 1 -type d -print0 | while IFS= read -r -d '' i; do
+    ./mds -vvv -s "$i" template.md
+    ./mds -vvvt -s "$i" template.md
     mv template.pdf testallresults/$(basename $i).pdf
     mv template_handout.pdf testallresults/$(basename $i)_handout.pdf
 done
